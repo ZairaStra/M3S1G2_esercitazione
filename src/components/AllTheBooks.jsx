60d435015483c629e7d6ea7886f9988244090ffb
ZairaStra <straticozaira@gmail.com>
2025-05-13 17:31:49 +0200

    btn

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -8,15 +8,17 @@ import scifi from "../data/scifi.json"; */
 const AllTheBooks = function () {
   return (
     <Container>
-      <Row className="justify-content-center gy-4 cols-2 cols-sm-3 cols-md-4 cols-lg-6">
+      <Row className="justify-content-center gy-4 cols-2 cols-md-3 cols-lg-4">
         {fantasy.map((book) => (
           <Col key={`fantasy-${book.asin}`}>
             <Card className="singleCard">
               <Card.Img className="singleCardImg" src={book.img} />
-              <Card.Body className="m-auto text-center">
-                <Card.Title className="singleCardTitle text-truncate fs-6">{book.title}</Card.Title>
-                <Card.Text>{book.category}</Card.Text>
-                <Button variant="danger">{book.price} €</Button>
+              <Card.Body className="text-center">
+                <Card.Title className="singleCardTitle text-truncate ">{book.title}</Card.Title>
+                <Card.Text className="singleCardText">{book.category}</Card.Text>
+                <Button className="singleCardButton" variant="danger">
+                  {book.price} €
+                </Button>
               </Card.Body>
             </Card>
           </Col>
