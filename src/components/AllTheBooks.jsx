c87863f64a952e72399946d8a59ab5403fbf9b5a
ZairaStra <straticozaira@gmail.com>
2025-05-13 17:53:34 +0200

    title text

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -8,11 +8,11 @@ import scifi from "../data/scifi.json"; */
 const AllTheBooks = function () {
   return (
     <Container>
-      <Row className="justify-content-center gy-4 cols-2 cols-md-3 cols-lg-4">
+      <Row xs={1} sm={2} md={3} lg={4} xxl={6} className="justify-content-center gy-4">
         {fantasy.map((book) => (
           <Col key={`fantasy-${book.asin}`}>
             <Card className="singleCard">
-              <Card.Img className="singleCardImg" src={book.img} />
+              <Card.Img className="singleCardImg" variant="top" src={book.img} />
               <Card.Body className="text-center">
                 <Card.Title className="singleCardTitle text-truncate">{book.title}</Card.Title>
                 <Card.Text className="singleCardText">{book.category}</Card.Text>
